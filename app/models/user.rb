86f0e3665a48ad139b802b2c381838d78baed6f8
touimiwa <toy10078toy@icloud.com>
2020-10-15 12:19:36 +0900

    見た目変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   belongs_to :user
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
+
+  validates :nickname, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
