1900619e3d9110f6a634a7d1be4744c89f2cbad9
touimiwa <toy10078toy@icloud.com>
2020-10-21 15:20:53 +0900

    管理者権限者変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   has_many :foots
   has_many :reviews, dependent: :destroy
 
-  # validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
 
-  # validates :nickname, presence: true
+  validates :nickname, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
